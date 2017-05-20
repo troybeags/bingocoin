2b75a90baae831ea2b410bd10860d24a426227b1
troybeags <troy.beagley@outlook.com>
2017-05-20 16:20:09 +0930

    Fix POS Reward

--- src/main.cpp
+++ src/main.cpp
@@ -1004,7 +1004,9 @@ int64_t GetProofOfWorkReward(int64_t nFees, int nHeight)
 int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees, int nHeight)
 {
 
-    int64_t nSubsidy = 5 * COIN;
+    int64_t nSubsidy;
+
+    nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8);
 
     LogPrint("creation", "GetProofOfStakeReward(): create=%s nCoinAge=%d nHeight=%d\n", FormatMoney(nSubsidy), nCoinAge, nHeight);
 
